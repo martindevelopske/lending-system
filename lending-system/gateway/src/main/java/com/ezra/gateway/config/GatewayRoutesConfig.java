0a684eed7ab7d24562bc443d7f7f10e03d12560a
Martin Ndung'u <martin.ndungu@caavagroup.com>
2026-03-10 16:55:45 +0300

    gateway module setup and configuration

---
+++ lending-system/gateway/src/main/java/com/ezra/gateway/config/GatewayRoutesConfig.java
@@ -0,0 +1,27 @@
+package com.ezra.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRoutesConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("product-service", r -> r
+                        .path("/api/products/**")
+                        .uri("lb://product-service"))
+                .route("customer-service", r -> r
+                        .path("/api/customers/**")
+                        .uri("lb://customer-service"))
+                .route("loan-service", r -> r
+                        .path("/api/loans/**")
+                        .uri("lb://loan-service"))
+                .route("notification-service", r -> r
+                        .path("/api/notifications/**", "/api/templates/**")
+                        .uri("lb://notification-service"))
+                .build();
+    }
+}
